6735dcd24a2416fa3f7d2ef845a70937a1cb268a
Qnnie <qnniedev@gmail.com>
2019-06-14 17:59:26 -0700

    works for more images now

--- yandereScraper.js
+++ yandereScraper.js
@@ -7,29 +7,32 @@ const tags = [process.argv[2]];
 const searchOptions = {
     limit: process.argv[3],
     nsfw: true,
-    random: false,
+    random: true,
 }
 
 const yandereScrape = async () => {
-    let imageFolder = `${__dirname}/${tags}`;
+    let imageFolder = `${__dirname}/${tags[0]}`;
     const posts = await Booru.search('yande.re', tags, searchOptions); 
-
     //If Folder doesn't exist create one
     if (!fs.existsSync(imageFolder)) {
-        fs.mkdir(path.join(__dirname, `/${tags}`), {}, err => {
+        fs.mkdir(path.join(__dirname, `/${tags[0]}`), {}, err => {
             if (err) throw err;
-            console.log(`${tags} Folder Created`)
+            console.log(`${tags[0]} Folder Created`)
         });
     }
     
-    posts.forEach( async post => {
-        let image = URLtoImage(post.fileUrl);
+    let requestsLimit= 5;
+    let i = 0;
+    for (const post of posts) {
+        if (i % requestsLimit == 0) StallDownloader();
+
+        let image = ParseURLtoImage(post.fileUrl);
         let imagePath = (`${imageFolder}/${image}`);
-        
+
         //If File already exists dont do anything
         if (fs.existsSync(imagePath)) {
             console.log(`${imagePath} already exists`);
-            return;
+            continue;
         }
 
         try {
@@ -38,15 +41,18 @@ const yandereScrape = async () => {
         } catch (err) {
             console.error(err);
         }
-    });
+        i++;
+    }
 }
 
-const URLtoImage = fileUrl => {
+const ParseURLtoImage = fileUrl => {
     let parseAt = 5;
     for (let i=0; i<fileUrl.length; i++) {
         if (fileUrl[i] == '/') parseAt--;
         if (parseAt == 0) return fileUrl.slice(i+1);
     }
 }
+
+const StallDownloader = () => setTimeout(() => console.log('stall'), 1000);
 
 yandereScrape();
